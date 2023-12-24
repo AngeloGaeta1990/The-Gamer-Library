60b5b574ccc35d494ed9cafbf00e2680eb3d29d8
Marianna Sil <silvestro.marianna.ms@gmail.com>
2023-12-24 18:10:17 +0100

    feat: add navbar to base template

--- library/views.py
+++ library/views.py
@@ -6,7 +6,7 @@ from .models import Platform
 # Create your views here.
 class PlatformList(generic.ListView):
     context_object_name = 'platforms'
-    template_name = "platform_list.html"
+    template_name = "base.html"
 
     def get_queryset(self):
         # Query all platform instances, including subclasses
