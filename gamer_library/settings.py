1d192abc5baab142817a214da2f9ebd6ec10f017
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2024-01-13 13:34:27 +0000

    fix: adapt code cleaning to heroku

--- gamer_library/settings.py
+++ gamer_library/settings.py
@@ -17,7 +17,8 @@ if os.path.isfile('env.py'):
     import env
 
 
-env
+if os.path.isfile('env.py'):
+    env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
